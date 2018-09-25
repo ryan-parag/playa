469933457081baa193ef1f4ed9734e7b2ff8b1ab
Ryan <ryan@Ryans-iMac.local>
2018-09-25 09:59:52 -0400

    add prefixing

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('sass', function(){
+gulp.task('sass', function () {
   return gulp.src('src/scss/playa.scss')
     .pipe(sass())
-    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(cleanCSS({
+      compatibility: 'ie8'
+    }))
     .pipe(gulp.dest('dist/css'))
 });
 
