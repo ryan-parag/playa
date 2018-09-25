66e1ead7ed530772f4e298bbf4f8fde86076e876
Ryan <ryan@Ryans-iMac.local>
2018-09-25 09:34:44 -0400

    Added components

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('sass', function(){
   return gulp.src('src/scss/playa.scss')
     .pipe(sass())
+    .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('dist/css'))
 });
+
+gulp.task('default', ['sass']);
